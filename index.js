bd09a1991aa8131711690783aed8dd0e94ac8169
Caroline Ko <hancarol91@gmail.com>
2023-01-16 14:34:48 -0500

    commit tree 5f68123930f4fcff7d34153ca93aaf48b8d3646b

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser='Grace Hopper';
+const firstInitial = currentUser[0];
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const welcomeMessage=`Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
+const shortGreeting =`Welcome, ${firstInitial}!`;
